9b5ee2828455f840e8705fe0182b813c7faafbab
ChonkStine <justine.carbonel.92@gmail.com>
2022-07-18 11:24:45 +0800

    hehe

---
+++ tryings/src/App.js
@@ -0,0 +1,14 @@
+import SideNav from './sidenav';
+import Home from './home';
+import './global.scss';
+
+function App() {
+  return (
+    <div class='app'>
+      <SideNav />
+      <Home />
+    </div>
+  );
+}//app end
+
+export default App;
