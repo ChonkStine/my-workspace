9e63d13cc0c5d95ba2c47a5579426466ab2c4317
ChonkStine <justine.carbonel.92@gmail.com>
2022-11-30 01:38:42 +0800

    approval page and scss

--- tryings/src/approval/approval.js
+++ tryings/src/approval/approval.js
@@ -9,9 +9,33 @@ export default function approval() {
       
             
             <div class='approval-container'>
-                <div class='user'>
-                    <div class='pic'></div>
-                </div>
+               <div class='approval-upper'>
+                    <div class='user'>
+                        <div class='approval-pic'></div>
+                        <div class='approval-user'>
+                            <div class='approval-name'>Justine R-Jay Carbonel</div>
+                            <div class='approval-uid'>1902314 ll SEAITE</div>
+                        </div>
+                    </div>
+                    <div class='approval-ate'>November 18 2022</div>
+               </div>
+
+
+               <div class='approval-middle'>
+                    <div class='approval-purpose'>Purpose</div>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+               </div>
+
+               <div class='approval-bottom'>
+                    <h5>Details: </h5>
+                    <p>Building: J Building</p>
+                    <p>Room: J44</p>
+                    <p>Date: December 15, 2022</p>
+                    <p>Time: 9:00 AM - 11:00 AM</p>
+               </div>
+
             </div>
                     
         </div>
